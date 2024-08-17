024f3d1f38c3c4e11160c01acf225edf305b3eb5
Madina_DI <12mileik@rambler.ru>
2024-08-17 19:12:14 +0300

    finished navigation part and creation other part of html project, css and js also

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+        port:8080,
+		client: {
+			logging: 'info', 
+			overlay: true,   
+			progress: true,   
+		},
+        open: true,
+	},
+    module: {
+        rules: [
+        {
+            test: /\.s[ac]ss$/i,
+            use: [MiniCssExtractPlugin.loader, 
+                'css-loader', 
+                'sass-loader',
+            ],
+        },
+        {
+            test: /\.pug$/,
+            loader: 'pug-loader',
+        }, 
+    ],
+},
+plugins: [new MiniCssExtractPlugin(), 
+          new HtmlWebpackPlugin({
+        template: './src/index.pug',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: 'src/img', to: 'img' },
+            ],
+        }),
+    ],
+    optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerPlugin()],
+	},
+};
